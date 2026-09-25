fb3cb7679663e9997e13a501f54bb9b2fd06416a
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2026-09-25 16:10:27 +0200

    fix build

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,18 +1,23 @@
 const production = Bun.argv.includes('--prod')
 
-const build = Bun.spawnSync([
-	'bunx',
-	'vite',
-	'build',
-	'--config',
-	'vite.config.ts',
-	'--mode',
-	production ? 'production' : 'development',
-])
+const build = Bun.spawnSync(
+	[
+		'bunx',
+		'vite',
+		'build',
+		'--config',
+		'vite.config.ts',
+		'--mode',
+		production ? 'production' : 'development',
+	],
+	{ stdio: ['inherit', 'inherit', 'inherit'] },
+)
 
 if (build.exitCode !== 0) process.exit(build.exitCode)
 
-const declaration = Bun.spawnSync(['bunx', 'tsc', '--project', 'tsconfig.build.json'])
+const declaration = Bun.spawnSync(['bunx', 'tsc', '--project', 'tsconfig.build.json'], {
+	stdio: ['inherit', 'inherit', 'inherit'],
+})
 
 if (declaration.exitCode !== 0) process.exit(declaration.exitCode)
 
